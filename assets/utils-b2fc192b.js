96a50dd77fea4b4828ba729c98ca2c4c69963a83
Parsons <gongyingparsons@gmail.com>
2024-09-23 17:11:20 +0800

    deploy

---
+++ assets/utils-b2fc192b.js
@@ -0,0 +1,2 @@
+const t=(e=0)=>new Promise(s=>setTimeout(s,e)),r={email:e=>/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)||"Please enter a valid email address",required:e=>!!e||"This field is required"};export{t as s,r as v};
+//# sourceMappingURL=utils-b2fc192b.js.map
